57eaf3d23c43ffa905f92ddf389b0c40237890dd
Hochfart0 <h_fanxujia@163.com>
2018-07-18 09:55:00 +0800

    vs git 尝试

--- 弹出组件/dialog.js
+++ 弹出组件/dialog.js
@@ -1,18 +1,31 @@
 define(['jquery-1.12.4'],function () {
     var dialog = {
-        open:function () {
+        open:function (arg) {
+            defaultArg = {
+                width:300,
+                height:300
+            };
+            $container = $('<div id="container"></div>');
+            
             var html ='<div id="container">'
                 +'<div id="background"></div>'
-                +'<div id="lgn">登录'
+                +'<div id="lgn">'
+                +'<span>登录</span>'
                 +'<span id="close">X</span>'
                 +'</div>'
                 +'</div>';
+            var options = $.extend(defaultArg,arg);
             $('body').append(html);
+            $('.dialog-content').css({
+                width:options.width,
+                height:options.height
+            });
+            $('.dialog')
         },
         close:function () {
 
         }
 
-    }
+    };
     return dialog;
-})
+});
---
+++ 弹出组件/index.js
@@ -0,0 +1,5 @@
+require(['dialog',['jquery-1.12.4']],function (dialog) {
+    $('#btn').on('click',function () {
+        dialog.open();
+    })
+})
